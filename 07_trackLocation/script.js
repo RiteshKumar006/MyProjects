dad0534259ae79d359d106d116a4cae53fcb89da
Ritesh Kumar <ritesh.jangra@meetribbon.com>
2024-04-13 14:10:42 +0530

    Track Locagtion project

---
+++ 07_trackLocation/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (e) {
+        form.classList.remove('hidden')
+      //   console.log('Map Event', e);
+      //   L.marker([e.latlng.lat, e.latlng.lng])
+      //     .addTo(map)
+      //     .bindPopup(L.popup({
+      //       maxWidth:250,
+      //       minWidth:100,
+      //       autoClose:false,
+      //       closeOnClick:false
+      //     }))
+      //     .setPopupContent()
+      //     .openPopup();
+      });
+    },
+    function () {
+      //   console.log('');
+      alert('Could not get your Position');
+    }
+  );
+}
