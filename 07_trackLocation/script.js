be26f719dbc6992bcaa2a93e670f38040d400870
Ritesh Kumar <ritesh.jangra@meetribbon.com>
2024-04-18 15:51:59 +0530

    18-04-2024

--- 07_trackLocation/script.js
+++ 07_trackLocation/script.js
@@ -11,33 +11,58 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 let map, mapEvent;
+console.log("cALUENOJADJ")
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    console.log("cxzxx")
+    this._getPosition()
+    console.log("this")
+  }
+
+  _getPosition() {
+    if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
+      alert("Could not get your Location")
+    })
+  }
+  }
+
+  _loadMap(position) {
+    console.log(position);
+    const { latitude, longitude } = position.coords;
+
+    this.#map = L.map('map').setView([latitude, longitude], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    L.marker([latitude, longitude])
+      .addTo(this.#map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
+    // Handling Click on Map
+    this.#map.on('click', function (mapE) {
+      this.#mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
+  }
+
+  _showForm() {}
+
+  _toggleElevationField() {}
+
+  _newWorkout() {}
+}
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log(position);
-      const { latitude, longitude } = position.coords;
-
-      map = L.map('map').setView([latitude, longitude], 13);
-
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      L.marker([latitude, longitude])
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
-
-
-      // Handling Click on Map  
-      map.on('click', function (mapE) {
-        mapEvent = mapE
-        form.classList.remove('hidden');
-        inputDistance.focus()
-     
-      });
-    },
+    function (position) {},
     function () {
       //   console.log('');
       alert('Could not get your Position');
@@ -45,37 +70,39 @@ if (navigator.geolocation) {
   );
 }
 
+const appMap = new App()
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
   event.preventDefault();
-    //Display Marker
-         console.log('Map Event', mapEvent,"inputDistance -- >", inputDistance.value);
-         L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
-           .addTo(map)
-           .bindPopup(L.popup({
-             maxWidth:250,
-             minWidth:100,
-             autoClose:false,
-             closeOnClick:false
-           }))
-           .setPopupContent()
-           .openPopup();
-           const items = JSON.parse(localStorage.getItem("userInfo")) || [];
+  //Display Marker
+  console.log('Map Event', mapEvent, 'inputDistance -- >', inputDistance.value);
+  L.marker([mapEvent.latlng.lat, mapEvent.latlng.lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+      })
+    )
+    .setPopupContent()
+    .openPopup();
+  const items = JSON.parse(localStorage.getItem('userInfo')) || [];
 
+  let obj = {
+    lat: mapEvent.latlng.lat,
+    lng: mapEvent.latlng.lng,
+    Distance: inputDistance.value,
+    Duration: inputDuration.value,
+  };
+  items.push(obj);
+  const stringObj = JSON.stringify(items);
 
-           let obj ={
-              "lat": mapEvent.latlng.lat,
-              "lng" : mapEvent.latlng.lng,
-              "Distance" : inputDistance.value,
-              "Duration" : inputDuration.value
-           }
-           items.push(obj)
-           const stringObj = JSON.stringify(items);
+  localStorage.setItem('userInfo', stringObj);
+});
 
-           localStorage.setItem("userInfo", stringObj )
-})
-
-inputType.addEventListener('change', function(){
+inputType.addEventListener('change', function () {
   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
+});
