fe32a80e90d54a45f0a5457675fc5b780e4a14dd
Ritesh Kumar <ritesh.jangra@meetribbon.com>
2024-06-25 16:11:25 +0530

    Tic Tac Toe

---
+++ 09_tic_tac_toe/src/assets/components/gameBoard.jsx
@@ -0,0 +1,17 @@
+const intialGameBoard = 
+[
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+
+const GameBoard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default GameBoard
