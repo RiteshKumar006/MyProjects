104dd47f3e01d619df1a73b272e550d42ae07595
Riteish <ritesh@Riteshs-MacBook-Air.local>
2024-07-14 11:48:07 +0530

    Added header

---
+++ 10_investment_calculator/src/component/Headers.jsx
@@ -0,0 +1,10 @@
+// import logo from '../assets/Investment-Calculator-logo';
+// const logo = await import('../assets/Investment-Calculator-logo')
+const Header =() =>{
+    return <header>
+        <img   alt='logo showing a money bag'/>
+        <h1>Investment Calculator</h1>
+    </header>
+}
+
+export default Header;
