7fd5e5ede087f041b69500efbe44279fd71df6bf
Ritesh <riteshjangra8@gmail.com>
2025-01-25 22:30:34 +0530

    Added new funtionality

--- MERNTodo/backend/routes/auth.js
+++ MERNTodo/backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user")
+const bcyptjs = require("bcryptjs")
 
 
 //SIGN in functionality
@@ -7,12 +8,35 @@ const User = require("../models/user")
 router.post("/register", async(req, res) =>{
     try{
         const {email,username, password} = req.body;
-        const user = new User({email,username, password});
+        const hashPassword = bcyptjs.hashSync(password)
+        const user = new User({email,username, password: hashPassword});
         await user.save().then(()=> res.status(200).json({user: user}))
         // console.log("user created", user)
     }catch(error){
         console.log(error)
         res?.status(400).json({error: error.message})
+    }
+})
+
+
+router.post("/singin", async(req,res)=>{
+    try{
+        const userFound = await User.findOne({email: req.body.email})
+    console.log("req", JSON.stringify(req.body), userFound.password)
+    if(!userFound){
+        res.status(400).json({message: "Please Signup first"})
+    }
+
+    const isPasswordCorrect = bcyptjs.compareSync(req?.body?.password, userFound?.password)
+    if(!isPasswordCorrect){
+        res.status(400).json({message: "Please enter a valid password"})
+    }
+
+    const {password, ...others} = userFound._doc;
+    res.status(200).json({others})
+    }catch(error){
+        console.log(error)
+
     }
 })
 
---
+++ MERNTodo/backend/routes/list.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require("../models/list");
+const { json } = require("express");
+
+//create task
+router.post("/addTask", async (req, res) => {
+  try {
+    const { title, email, body } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = new List({ title, body, user: existingUser });
+      await list.save().then(() => res.status(200).json({ list }));
+      existingUser.list.push(list);
+      existingUser.save();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//update title
+
+router.put("/updateTask/:id", async (req, res) => {
+  try {
+    const { title, email, body } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        console.log('find user',req.params.id)
+      // const list = new List({title, body, user: existingUser })
+      const list = await List.findByIdAndUpdate(req.params.id, { title, body });
+      list .save().then(() => res.status(200).json({message: "Task Updated"}));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//delete task
+router.delete("/deleteTask/:id", async (req, res) => {
+    try {
+      const { email} = req.body;
+  
+      const existingUser = await User.findOneAndDelete({ email }, {$pull: {list : req.params.id}});
+      if (existingUser) {
+          console.log('find user',req.params.id)
+        // const list = new List({title, body, user: existingUser })
+        await List.findByIdAndDelete(req.params.id ).then(() => res.status(200).json({message: "Task deleted"}))
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  //getTask
+  router.get("/getTasks/:id",async(req, res)=>{
+    const list = await List.find({user: req.params.id});
+    // const taskList = JSON.stringify(list)
+    res.status(200).json({list})
+    console.log("lit", list)
+
+  })
+module.exports = router;
