8ac2997319698dbe73ac4d9c2517d5521e2ae961
Ritesh <riteshjangra8@gmail.com>
2025-04-03 00:00:33 +0530

    frontend preparation

--- ecommerce-project/frontend/src/App.jsx
+++ ecommerce-project/frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection';
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <>
-      
+     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] bg-red'>
+     <Routes>
+        <Route path='/' element={<Home />}> </Route>
+        <Route path='/collection' element={<Collection />}> </Route>
+        <Route path='/about' element={<About />}> </Route>
+        <Route path='/contact' element={<Contact />}> </Route>
+     </Routes>
+     </div>
     </>
   )
 }
--- ecommerce-project/frontend/src/main.jsx
+++ ecommerce-project/frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from "react-router-dom"
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>,
+  </Router>,
 )
